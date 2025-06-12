7a581c2c3627d86384a9f92a85fcfbd5da1e3a7a
Gabrieltupi <gabrielsouza.n19@gmail.com>
2025-06-12 18:08:49 -0300

    fix:removendo vercel

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,30 @@
 require('dotenv').config();
-const express =require('express');
+const express = require('express');
 const cors = require('cors');
-const {Sequelize} = require('sequelize');
 const db = require('./models');
-const app= express();
-const PORT = process.env.PORT || 3000;
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const messagesRouter =require('./routes/messages');
+const messagesRouter = require('./routes/messages');
 app.use('/messages', messagesRouter);
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
 
 db.sequelize.authenticate()
   .then(() => {
     console.log('Conectado ao banco de dados Postgres com sucesso!');
-    return db.sequelize.sync();  
-  })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando na porta ${PORT}`);
-    });
+    return db.sequelize.sync();
   })
   .catch(err => {
     console.error('Erro ao conectar ao banco:', err);
   });
 
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando localmente na porta ${PORT}`);
+  });
+}
+
+module.exports = app;
