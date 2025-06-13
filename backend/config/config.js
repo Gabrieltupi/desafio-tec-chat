0c426b977e8aab43113dd7184e8f17ed47d1eb55
Gabrieltupi <gabrielsouza.n19@gmail.com>
2025-06-13 01:29:37 -0300

    fix:ajuste intencional afim de configurar a hospedagem backend4

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,18 +2,14 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
-      family: 4 
-    }
+    },
+    logging: console.log,
   }
 };
