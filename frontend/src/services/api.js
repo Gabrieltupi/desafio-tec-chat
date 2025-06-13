e91073a461b9e41e7bb55fe2245cc2eec6027c7f
Gabrieltupi <gabrielsouza.n19@gmail.com>
2025-06-13 01:49:44 -0300

    feat:link hospedagem backend para o frontend no ar!

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000'; 
+const API_URL = 'https://desafio-tec-chat-backend.onrender.com'; 
 
 export const getMessages = async () => {
   const res = await axios.get(`${API_URL}/messages/`);
